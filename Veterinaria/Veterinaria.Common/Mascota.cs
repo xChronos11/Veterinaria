0bc20af54ef6cfe7bae6aa5afa0da3912a2cbca8
xChronos11 <FranciscoDiaz-xChronos11@users.noreply.github.com>
2020-05-18 16:45:07 -0700

    Proyecto inicial veterinaria

---
+++ Veterinaria/Veterinaria.Common/Mascota.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veterinaria.Common
+{
+   public class Mascota
+    {
+        public int Id { get; set; }
+        public string Nombre {  get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
